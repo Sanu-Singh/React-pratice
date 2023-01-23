69b33cfb92b45597ea6b868ea4fad8280f501d68
Sanu Singh Rajput <81958742+Sanu-Singh@users.noreply.github.com>
2023-01-24 02:08:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Apicalling from './Pratice/Apicalling';
+import Hook from "./Pratice/Hook";
+function App() {
+  return (
+    <div className="App">
+     {/* <p>Hello React😍🤩</p> */}
+    
+    {/* <Hook/> */}
+    <Apicalling/>
+    </div>
+  );
+}
+
+export default App;
